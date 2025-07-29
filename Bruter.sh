cf8769a84d97a1048ad89a80ade5d398163a3670
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-07-29 10:27:01 +0800

    Update Bruter.sh

--- Bruter.sh
+++ Bruter.sh
@@ -29,22 +29,49 @@ echo "Open ports detected: $ports"
 # Check for SSH (22) or Telnet (23)
 if [[ $ports == *"22"* ]] || [[ $ports == *"23"* ]]; then
     echo "SSH (22) or Telnet (23) detected!"
-    
+
     # Prompt user for action
     read -p "Do you want to run bruteforce test? (y/n): " choice
-    
+
     if [[ $choice =~ [yY] ]]; then
+
+        # Defaults
+        prt=22
+        scrpt=ssh-brute
+        udb=users.txt
+        pdb=passwords.txt
+        output=logs.txt
+
         # Get script parameters
-        read -p "Enter port to test e.g 22 or 23: " port
-        read -p "Enter script name e.g. ssh-brute, telnet-brute: " script
-        read -p "Enter path to userdb file e.g user.txt: " userdb
-        read -p "Enter path to passdb file: e.g pass.txt: " passdb
-        read -p "Enter output filename e.g log.txt: " output_name
-        
+        echo "Enter port to test e.g 22 or 23: Enter to use by default"
+        read -r -p "> $prt " PRT
+        port="${PRT:-$prt}"
+
+
+        echo "Enter script name e.g. ssh-brute, telnet-brute: Enter to use by default"
+        read -r -p "> $scrpt " SCRPT
+        script="${SCRPT:-$scrpt}"
+
+
+        echo "Enter path to userdb file e.g user.txt: Enter Router Gateway IP: Enter to use by default"
+        read -r -p "> $udb " UDB
+        userdb="${UDB:-$udb}"
+
+
+        echo "Enter path to passdb file: e.g pass.txt: Enter Router Gateway IP: Enter to use by default"
+        read -r -p "> $pdb " PDB
+        passdb="${PDB:-$pdb}"
+
+
+        echo "Enter output filename e.g log.txt: Enter Router Gateway IP: Enter to use by default"
+        read -r -p "> $output " OUTPUT
+        output_name="${OUTPUT:-$output}"
+
+
         # Run the detailed scan
         echo "Running bruteforce test against port $port..."
         nmap -p $port --script $script --script-args userdb="$userdb",passdb="$passdb" "$target" -oN "$output_name" -vv
-        
+
         echo "Scan completed. Results saved to $output_name"
     else
         echo "Skipping bruteforce test."
